44a21f38f98f2de5b7341e58c1175572bd179f1c
seognil LC <seognil@gmail.com>
2022-12-01 13:24:53 +0800

    feat(add): remove grayscale

---
+++ src/remove-grayscale.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         [LC] Remove GrayScale
+// @description  remove css grayscale for usability
+// @version      beta
+// @author       Seognil LC
+// @license      AGPL-3.0-only
+// @namespace    https://github.com/seognil/my-web-user-scripts
+// @supportURL   https://github.com/seognil/my-web-user-scripts
+// @updateURL    https://raw.githubusercontent.com/seognil/my-web-user-scripts/master/src/remove-grayscale.user.js
+// @downloadURL  https://raw.githubusercontent.com/seognil/my-web-user-scripts/master/src/remove-grayscale.user.js
+// @match        *
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+{
+  const reverseGrayscale = () => {
+    domObserver("*", (node) => {
+      const style = getComputedStyle(node);
+      ["filter", "-webkit-filter", "-moz-filter", "-ms-filter", "-o-filter"].some((prop) => {
+        if (style[prop]?.match(/grayscale\(.*?\)/gi)) {
+          console.debug("[reverse grayscale]", node);
+          node.style.setProperty(prop, style[prop].replace(/grayscale\(.*?\)/gi, "grayscale(0)"), "important");
+        }
+      });
+    });
+  };
+
+  // * ----------------------------------------------------------------
+
+  const domObserver = (selector, callback) => {
+    // * already loaded
+    document.querySelectorAll(selector).forEach((node) => callback(node, "existed"));
+
+    // * keep observering new nodes if match selector
+    const observer = new MutationObserver((mutationList, observer) => {
+      mutationList.forEach((mutation) => {
+        mutation.addedNodes.filter((node) => node.matches?.(selector)).forEach((node) => callback(node, "new"));
+      });
+    });
+    observer.observe(document, { attributes: false, childList: true, subtree: true });
+  };
+
+  // * ----------------------------------------------------------------
+
+  const main = () => {
+    reverseGrayscale();
+  };
+  main();
+}
